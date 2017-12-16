2fc673c3880d60db7136a56d6202c6200289c831
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-12-16 13:27:28 -0200

    add day 5 part 2 solution

---
+++ 5/trampolines_maze_part_2.py
@@ -0,0 +1,29 @@
+
+def walk_maze(maze):
+    num_steps = 0
+    i = 0
+
+    while True:
+        try:
+            jump_inc = maze[i]
+            if jump_inc >= 3:
+                    maze[i] -= 1
+            else:
+                maze[i] += 1
+
+            num_steps += 1
+            i += jump_inc
+        except IndexError:
+            return num_steps
+
+def main():
+    with open('part_1_test_case.txt', 'r') as f:
+        maze = [int(row.split()[0]) for row in f]
+        assert walk_maze(maze) == 10
+
+    with open('input.txt', 'r') as f:
+        maze = [int(row.split()[0]) for row in f]
+        print("Answer: ", walk_maze(maze))
+
+if __name__ == '__main__':
+    main()
